8343da6af31701b9a458319a79da5c711c53216d
Hao Luo <luo1993826@gmail.com>
2019-01-02 23:22:17 -0800

    Changed file name to prevent import conflict

--- src/com/demo/ClientApi.java
+++ src/com/demo/ClientApi.java
@@ -1,21 +1,19 @@
 package com.demo;
 
 import java.util.ArrayList;
-
+import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
 
-public class Client {
-	
-	public static ArrayList<DataModel> getAllList(javax.ws.rs.client.Client c1)
+public class ClientApi {
+	public static ArrayList<DataModel> getAllList(Client c1)
 	{
 		Response r1 = c1.target("http://localhost:8080/movie/service/get/list").request().get();
 		ArrayList<DataModel> l2 = r1.readEntity(ArrayList.class);
 		return l2;
 	}
 	
-	public static Object getByorder(javax.ws.rs.client.Client c1, String order, String value)
+	public static Object getByorder(Client c1, String order, String value)
 	{
 		String url = "http://localhost:8080//movie/service/get/list?orderby="+order+"&value="+value;
 		Response r1 = c1.target(url).request().get();
@@ -24,7 +22,7 @@ public class Client {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		javax.ws.rs.client.Client c1 = ClientBuilder.newClient();
+		Client c1 = ClientBuilder.newClient();
 		/*These lines of code are using for checking whether each HTTP method works
 		 *under Jersey client API
 		 */
@@ -40,5 +38,4 @@ public class Client {
 		//test for post method with parameter (name = "aaa", year=2014, genre="Horror",watched=false)
 		
 	}
-
 }
